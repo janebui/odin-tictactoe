23bfd99f6e4b5dd95872f9c9d2e5893ed12d8d87
Jane <47288147+janebui@users.noreply.github.com>
2021-07-05 09:21:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,106 +1,196 @@
-const form = document.querySelector('form');
-const newbookBtn = document.querySelector('#newbook');
-const input = document.querySelector('.title');
-const add = document.querySelector('#add');
-const list = document.querySelector('#booklist');
-const clear = document.querySelector('#clear');
+// Player-making factory
+const Player = (name, marker) => {
+    return {
+        name,
+        marker
+    };
+};
 
 
-let myLibrary = [];
+// gameBoard module
+const gameBoard = (function () {
+    'use strict';
 
-myLibrary.push(new Book("The Tipping Point", "Malcolm Gladwell", 180, 0))
-myLibrary.push(new Book("Pride & Prejudice", "Jane Austen", 300, 1))
-myLibrary.push(new Book("Odyssey", "Homer", 200, 0))
+    const X = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve" class="svg-height">  <g><line class="marker x" x1="9" y1="90" x2="90" y2="9"/><line class="marker x" x1="9" y1="9" x2="90" y2="90"/></g></svg>'
 
-// Constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+    const O = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve" class="svg-height"><circle class="marker" cx="49.5" cy="49.5" r="40.5"/></svg>'
 
-Book.prototype.readToggle = function() {
-    this.read = !this.read;
-    console.log("Did you really read it? " + this.read);
-}
+    let human = Player('Human', X);
+    let computer = Player('Computer', O)
+    let currentPlayer = human;
+    
+    let gameActive = true;
+    let gameResult = 0; // 0 = no winner yet, 1 = someone won, 2 = draw
 
-// when the "read" checkbox gets clicked, call the prototype method readToggle
-function checkToggle(e) {
-    if (!e.target.matches('input[type="checkbox"]')) {
-        return;
+    const nameInput = document.querySelector('#name-input');
+    const restartBtn = document.querySelector('#restart');
+
+    // initialize game
+    let game = new Array(9).fill('');
+
+    // win conditions
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function escapeRegExp(string) {
+        return string.replace(/[.*+?^${}()|<>[\]\\]/g, ''); 
+      }      
+
+    function handleName(e) {
+        e.preventDefault();
+        human.name = escapeRegExp(document.getElementById('name').value);
+        console.log(human.name);
+        document.getElementById('your-name').innerHTML = human.name
+        this.reset();
     }
-    else {
-        const i = e.target.parentNode.dataset.index;
-        myLibrary[i].readToggle();
+
+    // when a cell is clicked, check if it has been marked already, otherwise mark the cell 
+    function handleCellClick(e) {
+        let clickedCell = e.target;
+        let clickedCellIndex = clickedCell.getAttribute('data-index');
+
+        // check if cell has already been marked
+        if (game[clickedCellIndex] !== '' || !gameActive) {
+            return;
+        } else { // mark the empty cell
+            game[clickedCellIndex] = human.marker;
+            clickedCell.innerHTML = human.marker;
+
+            handleResultValidation();
+            if (gameActive) {
+                handlePlayerChange();
+            }
+        }
     }
-}
 
-// eescape input using regex
-function escapeRegExp(string) {
-    return string.replace(/[.*+?^${}()|<>[\]\\]/g, ''); 
-  }  
+    function moveComputer() {
+        // find the cells that are still available
+        let avail = [];
+        let computerChoice;
 
-// create a new instance
-function addBook(e) {
-    e.preventDefault();
+        // record the indices of available cells into avail array
+        game.reduce(function (a, c, i) {
+            if (c == '') a.push(i)
+            return a;
+        }, avail);
 
-    let title = escapeRegExp(document.getElementById('title').value);
-    let author = escapeRegExp(document.getElementById('author').value);
-    let pages = escapeRegExp(document.getElementById('pages').value);
-    let read = document.getElementById('read').checked ? true : false;
+        // pick a random cell from the cells that are available
+        computerChoice = avail[Math.floor(Math.random() * avail.length)];
 
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+        game[computerChoice] = computer.marker;
+        handleResultValidation();
+        displayController.displayBoard(game);
 
-    displayBooks(myLibrary, list);
+        // if computer didn't win this turn, go to human's turn
+        if (gameActive) {
+            handlePlayerChange();
+        }
+    }
 
-    this.reset();
-}
+    // changes player after a player has taken a turn
+    function handlePlayerChange() {
+        if (currentPlayer == human) {
+            currentPlayer = computer;
+            moveComputer();
+        } else {
+            currentPlayer = human;
+        }
+        displayController.displayStatus(currentPlayer, gameResult);
+    }
 
-// toggle display of Add a New Book Form
-newbookBtn.addEventListener('click', e => {
-    document.querySelector('form').classList.toggle('hide')
-})
+    // check if anybody won yet
+    function handleResultValidation() {
+        for (let i = 0; i < winConditions.length; i++) {
 
-// populate list of books to be displayed
-function displayBooks(myLibrary, list) {
-    list.innerHTML = myLibrary.map((book, i) => {
+            const checkWin = winConditions[i]; // eg [2, 4, 6]
+            let a = game[checkWin[0]];
+            let b = game[checkWin[1]];
+            let c = game[checkWin[2]];
 
-        return `
-        <li data-index="${i}">
-        <div>${book.title}</div>
-        <div>${book.author}</div>
-        <div>${book.pages} ${book.pages == ""? "" : " pages"}</div>
-        
-        <input type="checkbox" id="item${i}" ${book.read == true ? "checked" : ""}/>
-        <label for="item${i}"></label>
-        <span class="delete">Delete</span>
-        
-        </li>
-        <hr>
-        `;
-    }).join('');
+            // skip win condition if not all 3 cells marked
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
 
-}
+            // when all 3 cells of win condition are marked, check if the same marker
+            if (a === b && b === c) {
+                gameActive = false;
+                gameResult = 1;
+                break;
+            } else if (!game.includes('')) {
+                gameActive = false;
+                gameResult = 2;
+            }
+        }
+        displayController.displayStatus(currentPlayer, gameResult);
+    }
 
-// delete target book when user clicks on delete button
-function deleteBook(e) {
-    if(!e.target.matches('.delete')) return;
-    myLibrary.splice(e.target.parentNode.dataset.index, 1);
-    displayBooks(myLibrary, list);
-}
+    function restartGame() {
+        game.fill('');
+        gameActive = true;
+        gameResult = 0;
+        currentPlayer = human;
+        displayController.displayBoard(game);
+        displayController.displayStatus(human, gameResult);
+    }
 
-// clear all books from the list
-function clearList(e) {
-    myLibrary = [];
-    list.innerHTML = '';
-}
+    nameInput.addEventListener('submit', handleName);
+    restartBtn.addEventListener('click', restartGame);
 
-form.addEventListener('submit', addBook);
+    return {
+        game,
+        gameResult,
+        currentPlayer,
+        handleCellClick
+    };
 
-displayBooks(myLibrary, list);
+})();
 
-list.addEventListener('click', deleteBook);
-list.addEventListener('click', checkToggle);
-clear.addEventListener('click', clearList)
+
+// display module
+const displayController = (function () {
+    const boardDisplay = document.querySelector('#gameboard');
+    const statusDisplay = document.querySelector('#game-status');
+    const section = document.querySelector('section');
+
+    const playerNode = document.createElement('p');
+    const playerTextNode = document.createTextNode('');
+    playerNode.appendChild(playerTextNode);
+    section.appendChild(playerNode);
+
+    function displayBoard(game) {
+        boardDisplay.innerHTML = game.map((item, i) => {
+            return `<div data-index="${i}" class="cell">${item}</div>`
+        }).join('');
+
+        document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', gameBoard.handleCellClick));
+    };
+    displayBoard(gameBoard.game);
+
+    function displayStatus(currentPlayer, gameResult) {
+        switch (gameResult) {
+            case 0:
+                statusDisplay.textContent = `It's ${currentPlayer.name}'s turn.`;
+                break;
+            case 1:
+                statusDisplay.textContent = `${currentPlayer.name} wins!`;
+                break;
+            case 2:
+                statusDisplay.textContent = "Draw";
+                break;
+        }
+    }
+
+    return {
+        displayStatus,
+        displayBoard
+    }
+})();
